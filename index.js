1e5fb0a89de3635d2de619cf28affcc4c5c93a3c
boumex <64454765+boumex@users.noreply.github.com>
2020-05-12 17:55:27 +0200

    Now it works as intended

--- index.js
+++ index.js
@@ -13,15 +13,17 @@ var form = JAK.gel("form");
 JAK.Events.addListener(form, "submit", geokoduj); /* Při odeslání formuláře spustit geokódování */
 
 function geokoduj(e, elm) {  /* Voláno při odeslání */
-    
+
+    console.log(query.length); // TADY loguju délku query jestli je správně dlouhá
+
     for (j = 0; j <= query.length-1; j++) {
         JAK.Events.cancelDef(e); /* Zamezit odeslání formuláře */
-        new SMap.Geocoder(query[j], odpoved);
+        new SMap.Geocoder(JAK.gel("query"+(1+j)).value, odpoved);
     }
 }
 
 function odpoved(geocoder) { /* Odpověď */
-    if (i == query.length) {
+    if (i >= query.length) {
         souradnice = [];
         i = 0;
     }
@@ -30,11 +32,13 @@ function odpoved(geocoder) { /* Odpověď */
         return;
     }
 
+    // posilam adresu serveru mapy
     var vysledky = geocoder.getResults()[0].results;
     var data = [];
     var item = vysledky.shift();
     data.push(item.coords.toWGS84(2).reverse().join(", "));
     
+    // preformatovani souradnic
     souradnice[i] = data[0].split(",");
     for(j = 0; j <= 1; j++) {
         var b = [];
@@ -49,14 +53,10 @@ function odpoved(geocoder) { /* Odpověď */
     }
     
     i++;
-    console.log(souradnice);
     prumer(souradnice);
 }
 
-
-
-
-function prumer(x){
+function prumer(x){ /* Prumer souradnic */
     if (i == query.length) {
         var y = [0, 0];
         var ql = query.length;
@@ -64,89 +64,52 @@ function prumer(x){
             y[0] += x[q][0];
             y[1] += x[q][1];
         }
-        window.open('https://mapy.cz/zakladni?q='+y[0]/ql + "N, " + y[1]/ql + "E", '_blank')
+        //console.log('https://mapy.cz/zakladni?q='+"hospoda "+y[0]/ql + "N, " + y[1]/ql + "E");
+        window.open('https://mapy.cz/zakladni?q='+"hospoda "+y[0]/ql + "N, " + y[1]/ql + "E", '_blank')
     }
     location.reload;
 }
-   
-
-function fce_add(element){
-    var form = document.getElementById("form");
-    // Create a new HTML tag of type "input"
-    var field = document.createElement("input");
-    // The value filled in the form will be stored in an array
-    field.name = "query[]";
-    field.type = "text";
-    
-    var rem = document.createElement("input");
-
-    // We create a new element of type "p" and we insert the field inside.
-    var bloc = document.createElement("p");
-    bloc.appendChild(field);
-    form.insertBefore(add, element);
-    form.insertBefore(rem, element);
-    form.insertBefore(bloc, element);
-}
-
-function fce_remove(element){
-    var form = document.getElementById("form");
-    // Remove the field
-    form.removeChild(element.nextSibling);
-}
 
 
-function pridat() {
+function pridat() { /* Pridat dalsi adresu */
     n = query.length+1;
-    q = document.getElementById("query2").value;
-    label = document.createElement("label");
-    inp = document.createElement("input");
-    inp.innerHTML.value = q;
-    inp.innerHTML.id = "query"+n;
-    tr.appendChild(td1);
-    tr.appendChild(td2);          
-    document.getElementById("tab1").appendChild(tr);
+    var p = document.getElementById("adresy");
+    var label = document.createElement("label");
+   
+    label.innerHTML = n+". adresa: ";
 
-    query += JAK.gel("query"+n).value
+    p.appendChild(label);
+    parek = document.createElement("input");
+    label.appendChild(parek);
+    parek.setAttribute("id", "query"+n);
+    parek.setAttribute("type", "text");
+    parek.setAttribute("value", "");
+
+    newLine = document.createElement("br");
+    newLine2 = document.createElement("br");
+    label.appendChild(newLine);
+    label.appendChild(newLine2);
+
+    query[n-1] = JAK.gel("query"+n).value;
 }
 
 
-
-function pridat() {
-    txt1 = document.getElementById("t1").value;
-    txt2 = document.getElementById("t2").value;
-    tr = document.createElement("tr");
-    td1 = document.createElement("td");
-    td1.innerHTML = txt1;
-    td2 = document.createElement("td");
-    td2.innerHTML = txt2;
-    tr.appendChild(td1);
-    tr.appendChild(td2);          
-    document.getElementById("tab1").appendChild(tr);;
+function odebrat() { /* Odebrat posledn iadresu */
+    n = query.length;
+    if(n > 2) {
+        p = document.getElementById("adresy");
+        lastLabel = p.lastElementChild;
+        console.log(lastLabel.innerHTML);
+        lastInput = document.getElementById("query"+n);
+        lastLabel.removeChild(lastInput);
+        lastLabel.innerHTML = "";
+        p.removeChild(lastLabel);
+        query.pop();
+    }
 }
-function odebrat() {
-    tbl = document.getElementById("tab1");
-    lc = tbl.lastElementChild;
-    if(lc)
-      tbl.removeChild(lc);
-}
-
 
 
 /*
-function prevod(x) {
-    var y = [];
-    y[i] = x[0].split(",");
-    for(j = 0; j <= 1; j++) {
-        var b = [];
-        var c = 0;
-        y[i][j] = y[i][j].replace(/[NE"]/gi ,'');
-        b = y[i][j].split(/[°']/);
-        
-        for(k = 0; k <= 2; k++){
-            c += b[k]/Math.pow(60, k);
-        }
-        y[i][j] = c;
-    }
-    return y;
-}
+POZNAMKY K HTML
+- nejde nastavit promenliva vyska mapy, vzdycky se to spleskne
 */
